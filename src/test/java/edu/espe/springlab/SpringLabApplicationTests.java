02ff5e653dfc03f83ca4324866a00badbbb927c7
unknown <irvingmartinez2004@gmailcom>
2025-11-07 16:38:31 -0500

    Create a workflow for CI

--- src/test/java/edu/espe/springlab/SpringLabApplicationTests.java
+++ src/test/java/edu/espe/springlab/SpringLabApplicationTests.java
@@ -2,12 +2,13 @@ package edu.espe.springlab;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.yml")
 class SpringLabApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
